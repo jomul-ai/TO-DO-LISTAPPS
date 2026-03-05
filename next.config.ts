50403a6abb65448d24ea7a299a12efdff9c3eed7
jonathan mulyono <139569376+jomul-ai@users.noreply.github.com>
2026-03-05 17:00:46 +0700

    first commit

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        pathname: "/api/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+        search: "",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
